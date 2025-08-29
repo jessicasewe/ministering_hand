4f92190473df34035f56d9f2dc40725a879a796d
jessicasewe <jessicasewe89@gmail.com>
2025-08-29 16:07:57 +0000

    chore: update

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -41,7 +41,7 @@ const navigationItems: NavItem[] = [
     dropdown: [
       {
         title: "Our Mission and Vision",
-        href: "/about",
+        href: "/about#mission-vision",
         icon: <Target size={16} />,
       },
       { title: "Our Board", href: "/about#board", icon: <Users size={16} /> },
