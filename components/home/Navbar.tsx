823a2994f23c3906b7a014d56a3212e3855274f4
jessicasewe <jessicasewe89@gmail.com>
2025-10-09 18:23:06 +0000

    changes

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -350,9 +350,11 @@ export default function Navbar() {
               );
             })}
             <div className="pt-4">
-              <Button className="w-full border border-[#1e8e9d] bg-transparent text-[#1e8e9d] hover:bg-[#1e8e9d] hover:text-white px-8 py-3 rounded-full transition-colors font-medium">
-                Donate
-              </Button>
+              <Link href="/donate" passHref>
+                <Button className="w-full border border-[#1e8e9d] bg-transparent text-[#1e8e9d] hover:bg-[#1e8e9d] hover:text-white px-8 py-3 rounded-full transition-colors font-medium">
+                  Donate
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
